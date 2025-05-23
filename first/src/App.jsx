7a6ad8b5bd9bd59d5192be565c774e4cc78653af
Suraj Yadav <sy083609@gmail.com>
2025-05-23 17:55:35 +0530

    updated commit

--- first/src/App.jsx
+++ first/src/App.jsx
@@ -1,15 +1,29 @@
-import Header from "./Header.jsx"
-import Middle from "./Middle.jsx"
-import Footer from './Footer.jsx'
+// import Header from "./Header.jsx"
+// import Middle from "./Middle.jsx"
+// import Footer from './Footer.jsx'
+// const App=()=>{
+//   return(
+//         <>
+//       <Header/>
+//       <Middle/>
+//       <Footer/>
+
+//     </>
+    
+//   );
+// }
+// export default App;
+
+import College from "./College";
+const clgname="Sage University Bhopal";
+const address="Katara, Bhopal";
+const fees=50000;
 const App=()=>{
   return(
-        <>
-      <Header/>
-      <Middle/>
-      <Footer/>
-
+    <>
+    <h1 align="center">Welcome !!!</h1>
+    <College clg={clgname} add={address} fs={fees}/>
     </>
-    
-  );
+  )
 }
 export default App;
---
+++ first/src/Cybrom.jsx
@@ -0,0 +1,9 @@
+const Cybrom=(props)=>{
+    return(
+        <>
+        <h1>My name:{props.nm}</h1>
+        <h1>My City:{props.city}</h1>
+        </>
+    )
+}
+export default Cybrom;
