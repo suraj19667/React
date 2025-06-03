2105873c51514cba19303f3c0f53b5d6c4902d12
Suraj Yadav <sy083609@gmail.com>
2025-06-03 17:51:20 +0530

    updated commit

--- first/src/App.jsx
+++ first/src/App.jsx
@@ -1,25 +1,16 @@
-import React, { useState } from "react";
+import {useState} from "react";
+const App=()=>{
+  const [color,setColor]=useState("red")
+  return(
+    <>
+    <h1>Welcome to react App </h1>
+    <button onClick={()=>{setColor("yellow")}}>Change color</button>
+    <button onClick={()=>{setColor("blue")}}>Change color</button>
+    <button onClick={()=>{setColor("green")}}>Change color</button>
 
-function App() {
-  const [fact, setFact] = useState("");
 
-  const fetchFact = async () => {
-    try {
-      const response = await fetch("https://catfact.ninja/fact");
-      const data = await response.json();
-      setFact(data.fact);
-    } catch (error) {
-      setFact("Failed to fetch fact. Please try again.");
-    }
-  };
-
-  return (
-    <div style={{ textAlign: "center", marginTop: "100px" }}>
-      <h1>Random Cat Fact</h1>
-      <button onClick={fetchFact}>Get Fact</button>
-      <p style={{ marginTop: "20px" }}>{fact}</p>
-    </div>
-  );
+    <h2 style={{color:color}}>My Favorite Color :{color}</h2>
+    </>
+  )
 }
-
 export default App;
