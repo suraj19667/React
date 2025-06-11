9774bd1286e1e2ffaaed42e89730418761128568
Suraj Yadav <sy083609@gmail.com>
2025-06-11 17:26:12 +0530

    update commit

--- first/src/App.jsx
+++ first/src/App.jsx
@@ -1,16 +1,47 @@
-import {useState} from "react";
+
+// import { useState } from "react"
+// const App=()=>{
+//   const [name,setName]=useState("")
+//   const [city,setCity]=useState("")
+//   const handleSubmit=()=>{
+//     console.log({name:name,city:city})
+//   }
+//   return(
+//   <>
+//   <h1>Application Form</h1>
+
+//   Enter Name:<input type="text" value={name} onChange={(e)=>{setName(e.target.value)}}/>
+//   <br/>
+//   Enter City: <input type="text" value={city} onChange={(e)=>{setCity(e.target.value)}}/>
+//   <br/>
+//   <button onClick={handleSubmit}>Click</button>
+//   </>
+//   )
+// }
+// export default App;
+import { useState } from "react";
 const App=()=>{
-  const [color,setColor]=useState("red")
+ const [input,setInput]=useState({})
+
+ const handleInput=(e)=>{
+  let name=e.target.name;
+  let value=e.target.value;
+  setInput(values=>({...values,[name]:value}))
+  console.log(input)
+ }
   return(
     <>
-    <h1>Welcome to react App </h1>
-    <button onClick={()=>{setColor("yellow")}}>Change color</button>
-    <button onClick={()=>{setColor("blue")}}>Change color</button>
-    <button onClick={()=>{setColor("green")}}>Change color</button>
-
-
-    <h2 style={{color:color}}>My Favorite Color :{color}</h2>
+    Enter Rollno:<input type="text" name="rollno" onChange={handleInput}/>
+    <br/>
+    Enter Name:<input type="text" name="name" onChange={handleInput}/>
+    <br/>
+    Enter city:<input type="text" name="city" onChange={handleInput}/>
+    <br/>
+    Enter Fees:<input type="text" name="fees" onChange={handleInput}/>
+    <br/>
+    <button>click</button>
     </>
+
   )
 }
 export default App;
