af57d3f07cd8d5327d1cfd6525f2f2f3296c4725
Suraj Yadav <sy083609@gmail.com>
2025-07-05 14:02:43 +0530

    updated commit

--- first/src/App.jsx
+++ first/src/App.jsx
@@ -19,29 +19,3 @@ const App=()=>{
   )
 }
 export default App;
-// import { useState } from "react";
-// const App=()=>{
-//  const [input,setInput]=useState({})
-
-//  const handleInput=(e)=>{
-//   let name=e.target.name;
-//   let value=e.target.value;
-//   setInput(values=>({...values,[name]:value}))
-//   console.log(input)
-//  }
-//   return(
-//     <>
-//     Enter Rollno:<input type="text" name="rollno" onChange={handleInput}/>
-//     <br/>
-//     Enter Name:<input type="text" name="name" onChange={handleInput}/>
-//     <br/>
-//     Enter city:<input type="text" name="city" onChange={handleInput}/>
-//     <br/>
-//     Enter Fees:<input type="text" name="fees" onChange={handleInput}/>
-//     <br/>
-//     <button>click</button>
-//     </>
-
-//   )
-// }
-// export default App;
